5972aa1c93d7514c11e833a5da868c155ae7c600
Phillip Corey Roark <pr@coaching2100.com>
2024-12-31 15:05:45 -0600

    Update to enable Ray cluster monitoring

--- fix.sh
+++ fix.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Enable monitoring and logging
+# Enable Ray cluster monitoring
 gcloud container clusters update private-cluster-auto \
     --zone us-west1 \
     --project=api-for-warp-drive \
-    --monitoring=SYSTEM \
-    --logging=SYSTEM
+    --enable-ray-cluster-monitoring
 
 # Verify status
 gcloud container clusters describe private-cluster-auto \
